c21b0b2bc4a5a4c3c313587efd1030dd3f815bce
unknown <gujwarpooja@gmail.com>
2025-05-31 13:56:45 +0530

    Create a new repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter,Routes,Route, createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from './pages/Home';
+import GoogleProfile from './pages/GoogleProfile';
+import Albums from './pages/Albums';
+import Image from './pages/Image';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import UserContext from './context/UserContext';
+import {useState} from 'react'
+
+const router = createBrowserRouter([
+  {
+    path:"/",element:<Home/>
+  },
+  {
+path:"/v2/profile/google",element:<GoogleProfile/>
+  }
+  , {
+    path: '/v1/profile/google',
+    element: <GoogleProfile />,
+  },
+  {
+    path:"/albums",
+    element:<Albums/>
+  },
+  {
+    path:"/images",
+    element:<Image/>
+  }
+])
 
 function App() {
+  const [user,setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <UserContext.Provider value={{user,setUser}}>
+    <RouterProvider router={router}/>
+    </UserContext.Provider>
+    </>
   );
 }
 
---
+++ src/pages/GoogleProfile.js
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+//import Cookies from "js-cookie";
+import axios from "axios";
+import { useState, useEffect} from "react";
+import { useNavigate ,useLocation} from "react-router-dom";
+// import { authServerAxios, googleApiAxios } from "../lib/axios.lib";
+import {authServerAxios} from "../lib/axios.lib"
+import UserContext from "../context/UserContext";
+
+export default function GoogleProfile() {
+  
+  const location = useLocation()
+  console.log(location ,"LOCATION")
+  const navigate = useNavigate()
+  const {user,setUser} = useContext(UserContext)
+
+  
+  useEffect(() => {
+    (async () => {
+        if(location.pathname.includes('v2')) {
+        try {
+          const {data} = await authServerAxios.get('/user/profile/google',{withCredentials:true});
+          setUser(data)
+          navigate("/albums")
+        } catch (error) {
+          if (error.status === 403 || error.status === 500) {
+           // navigate("/")
+          }
+        }
+      } else {
+       // navigate("/")
+      }
+    })();
+  }, []);
+
+  return (
+    <header
+       style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "3rem 1rem",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2 style={{ fontSize: "2rem" }}>🎉 Welcome to Simbapix App 🎉</h2>
+      <p style={{ marginTop: "1rem", fontSize: "1.2rem" }}>
+        Explore your albums and enjoy your moments!
+      </p>
+    </header>
+    
+  );
+}
