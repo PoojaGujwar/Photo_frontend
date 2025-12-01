476eefebcec9d0c5959347ff54091e4cd1a35f5d
Pooja Gujwar <gujwarpooja@gmail.com>
2025-12-01 12:26:28 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {BrowserRouter,Router,Route,Routes} from "react-router-dom"
 import './App.css';
 import Home from "./components/Home";
@@ -32,53 +32,8 @@ function App() {
     </Routes>
     </BrowserRouter>
     </div>
-=======
+    )}
 
-import './App.css';
-import {BrowserRouter,Routes,Route, createBrowserRouter, RouterProvider} from "react-router-dom"
-import Home from './pages/Home';
-import GoogleProfile from './pages/GoogleProfile';
-import Albums from './pages/Albums';
-import Image from './pages/Image';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import UserContext from './context/UserContext';
-import {useState} from 'react'
 
-const router = createBrowserRouter([
-  {
-    path:"/",element:<Home/>
-  },
-  {
-path:"/v2/profile/google",element:<GoogleProfile/>
-  }
-  , {
-    path: '/v1/profile/google',
-    element: <GoogleProfile />,
-  },
-  {
-    path:"/albums",
-    element:<Albums/>
-  },
-  {
-    path:"/images",
-    element:<Image/>
-  }
-])
 
-function App() {
-  const [user,setUser] = useState(null)
-  if(user){
-    <Albums/>
-  }
-  return (
-    <>
-    <UserContext.Provider value={{user,setUser}}>
-    <RouterProvider router={router}/>
-    </UserContext.Provider>
-    </>
->>>>>>> 563016f0013d7b37c56339e3ab1508023d394a6d
-  );
-}
 
-export default App;
